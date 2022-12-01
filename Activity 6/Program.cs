6654f35217332cc8cac7ca2cba1b3b1c0bb7e952
TA1137 <muhammad.reza.ft21@mail.umy.ac.id>
2022-12-01 15:56:58 +0700

    Add Class Stacks

--- Activity 6/Program.cs
+++ Activity 6/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -16,6 +17,57 @@ namespace Activity_6
             {
                 info = i;
                 next = n;
+            }
+        }
+
+        class Stacks
+        {
+            Node top;
+
+            public Stacks()
+            {
+                top = null;
+            }
+
+            bool empty()
+            {
+                if (top == null)
+                    return (true);
+                else
+                    return (false);
+            }
+
+            public void push (int element)
+            {
+                Node fresh;
+                fresh = new Node(element, null);
+
+                fresh.next = top;
+                top = fresh;
+                Console.WriteLine("\n" + element + " pushed. ");
+            }
+
+            public void pop()
+            {
+                Console.WriteLine("\nThe popped element is: " + top.info);
+                top = top.next; //Make top point to the next node in sequence
+            }
+
+            public void display()
+            {
+                Node tmp;
+
+                if (empty())  //If stack is empty
+                    Console.WriteLine("\nStack Empty");
+                else
+                {
+                    //Traverse the list from beginning till end
+                    for (tmp = top; tmp != null; tmp = tmp.next)
+                    {
+                        Console.WriteLine(tmp.info);
+                    }
+                    Console.WriteLine();
+                }
             }
         }
 
