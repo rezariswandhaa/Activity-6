985cee01ad2978f2d5c94fdc511402133c1da753
TA1137 <muhammad.reza.ft21@mail.umy.ac.id>
2022-12-01 16:15:08 +0700

    Add Main

--- Activity 6/Program.cs
+++ Activity 6/Program.cs
@@ -7,73 +7,109 @@ using System.Threading.Tasks;
 
 namespace Activity_6
 {
-    internal class Program
+    class Node
     {
-        class Node
+        public int info;
+        public Node next;
+        public Node(int i, Node n)
         {
-            public int info;
-            public Node next;
-            public Node(int i, Node n)
-            {
-                info = i;
-                next = n;
-            }
+            info = i;
+            next = n;
+        }
+    }
+
+    class Stacks
+    {
+        Node top;
+
+        public Stacks()
+        {
+            top = null;
         }
 
-        class Stacks
+        bool empty()
         {
-            Node top;
+            if (top == null)
+                return (true);
+            else
+                return (false);
+        }
 
-            public Stacks()
+        public void push(int element)
+        {
+            Node fresh;
+            fresh = new Node(element, null);
+
+            fresh.next = top;
+            top = fresh;
+            Console.WriteLine("\n" + element + " pushed. ");
+        }
+
+        public void pop()
+        {
+            Console.WriteLine("\nThe popped element is: " + top.info);
+            top = top.next; //Make top point to the next node in sequence
+        }
+
+        public void display()
+        {
+            Node tmp;
+
+            if (empty())  //If stack is empty
+                Console.WriteLine("\nStack Empty");
+            else
             {
-                top = null;
-            }
-
-            bool empty()
-            {
-                if (top == null)
-                    return (true);
-                else
-                    return (false);
-            }
-
-            public void push (int element)
-            {
-                Node fresh;
-                fresh = new Node(element, null);
-
-                fresh.next = top;
-                top = fresh;
-                Console.WriteLine("\n" + element + " pushed. ");
-            }
-
-            public void pop()
-            {
-                Console.WriteLine("\nThe popped element is: " + top.info);
-                top = top.next; //Make top point to the next node in sequence
-            }
-
-            public void display()
-            {
-                Node tmp;
-
-                if (empty())  //If stack is empty
-                    Console.WriteLine("\nStack Empty");
-                else
+                //Traverse the list from beginning till end
+                for (tmp = top; tmp != null; tmp = tmp.next)
                 {
-                    //Traverse the list from beginning till end
-                    for (tmp = top; tmp != null; tmp = tmp.next)
-                    {
-                        Console.WriteLine(tmp.info);
-                    }
-                    Console.WriteLine();
+                    Console.WriteLine(tmp.info);
                 }
+                Console.WriteLine();
             }
         }
-
 
         static void Main(string[] args)
         {
+            Stacks s = new Stacks();
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push.");
+                Console.WriteLine("2. Pop");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\nEnter your choice");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a number: ");
+                        int num = Convert.ToInt32(Console.ReadLine());
+                        s.push(num);
+                        break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.pop();
+                        break;
+                    case '3':
+                        s.display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
+                        break;
+                }
+            }
         }
     }
+
+
+    
 }
